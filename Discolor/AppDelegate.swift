f536dd0aa074ffd49f54ad6042f4685866eaef57
Anthony Li <li.anthony917@gmail.com>
2020-04-20 13:36:47 -0400

    Add functionality

--- Discolor/AppDelegate.swift
+++ Discolor/AppDelegate.swift
@@ -7,18 +7,103 @@
 //
 
 import Cocoa
+import ApplicationServices
+import Quartz
+
+enum DiscordAppearance {
+    case light
+    case dark
+}
+
+enum ColorChangeError: Error {
+    case discordNotRunning
+}
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    static let autoChangeKey = "DiscolorAutomaticallyChangeOnAppearanceChange"
+    var item: NSStatusItem!
+    weak var window: NSWindow?
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        NSApp.setActivationPolicy(.accessory)
+        
+        item = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+        item.button?.title = "☯️"
+        item.button?.target = self
+        item.button?.action = #selector(show(sender:))
+        
+        NSApp.hide(nil)
+    }
+    
+    @objc func show(sender: Any?) {
+        NSApp.unhide(nil)
+        window?.level = .floating
+        window?.makeKeyAndOrderFront(nil)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        false
+    }
+        
+    func changeColor(to appearance: DiscordAppearance) throws {
+        guard let discord = NSRunningApplication.runningApplications(withBundleIdentifier: "com.hnc.Discord").first else {
+            throw ColorChangeError.discordNotRunning
+        }
+        
+        let pid = discord.processIdentifier
+        print("Discord's PID is \(pid)")
+        
+        let tab: CGKeyCode = 0x30
+        
+        let additionalTabs = appearance == .dark ? 11 : 12
+        var keys = [CGKeyCode]()
+        keys += [0xFF, 0x28, 0xFE, 0x35, 0xFF, 0x2B, 0xFE] // Open then close quick switcher, then open Preferences
+        keys += [CGKeyCode](repeating: tab, count: 16) // Tab to Appearance
+        keys += [0x31] // Open Appearance
+        keys += [CGKeyCode](repeating: tab, count: additionalTabs) // Tab to desired color scheme
+        keys += [0x31, 0x35] // Select and exit
+        var i = 0
+        
+        var process: (() -> Void)?
+        var flags = CGEventFlags()
+        process = {
+            let key = keys[i]
+            
+            var down: CGEvent?
+            var up: CGEvent?
+            
+            if key == 0xFF {
+                flags.insert(.maskCommand)
+            } else if key == 0xFE {
+                flags.remove(.maskCommand)
+            } else {
+                down = CGEvent(keyboardEventSource: nil, virtualKey: key, keyDown: true)!
+                up = CGEvent(keyboardEventSource: nil, virtualKey: key, keyDown: false)!
+            }
+            
+            down?.flags = flags
+            up?.flags = flags
+            
+            down?.postToPid(pid)
+            up?.postToPid(pid)
+            
+            i += 1
+            if i < keys.count {
+                if keys[i] != key {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.2, execute: process!)
+                } else {
+                    process!()
+                }
+            }
+        }
+        
+        process!()
     }
 
 
--- Discolor/ViewController.swift
+++ Discolor/ViewController.swift
@@ -7,13 +7,34 @@
 //
 
 import Cocoa
+import Combine
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, NSWindowDelegate {
+    
+    @IBOutlet weak var autoChangeSwitch: NSSwitch?
 
+    private var cancellables = Set<AnyCancellable>()
+    
+    var currentAppearance: NSAppearance.Name?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        currentAppearance = view.effectiveAppearance.bestMatch(from: [.darkAqua, .aqua])
+        
+        autoChangeSwitch?.state = UserDefaults.standard.bool(forKey: AppDelegate.autoChangeKey) ? .on : .off
 
-        // Do any additional setup after loading the view.
+        view.publisher(for: \.effectiveAppearance).sink { [unowned self] appearance in
+            let name = appearance.bestMatch(from: [.darkAqua, .aqua])
+            if name != self.currentAppearance {
+                self.currentAppearance = name
+                if UserDefaults.standard.bool(forKey: AppDelegate.autoChangeKey) {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                        try? (NSApp.delegate as! AppDelegate).changeColor(to: name == .darkAqua ? .dark : .light)
+                    }
+                }
+            }
+        }.store(in: &cancellables)
     }
 
     override var representedObject: Any? {
@@ -21,7 +42,32 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
+    
+    override func viewDidAppear() {
+        view.window?.delegate = self
+        view.window?.collectionBehavior.insert(.canJoinAllSpaces)
+        (NSApp.delegate as! AppDelegate).window = view.window
+    }
+    
+    @IBAction func light(sender: NSButton?) {
+        try? (NSApp.delegate as! AppDelegate).changeColor(to: .light)
+    }
 
-
+    @IBAction func dark(sender: NSButton?) {
+        try? (NSApp.delegate as! AppDelegate).changeColor(to: .dark)
+    }
+    
+    @IBAction func matchSystem(sender: NSButton?) {
+        try? (NSApp.delegate as! AppDelegate).changeColor(to: view.effectiveAppearance.bestMatch(from: [.darkAqua, .aqua]) == .darkAqua ? .dark : .light)
+    }
+    
+    @IBAction func autoChangeSwitched(sender: NSSwitch?) {
+        UserDefaults.standard.set(sender?.state == .on, forKey: AppDelegate.autoChangeKey)
+    }
+    
+    func windowShouldClose(_ sender: NSWindow) -> Bool {
+        NSApp.hide(nil)
+        return false
+    }
 }
 
