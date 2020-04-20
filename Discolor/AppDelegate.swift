73b967809093b8e748c7762205010a743adc16bd
Anthony Li <li.anthony917@gmail.com>
2020-04-20 08:48:01 -0400

    Initial Commit

---
+++ Discolor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Discolor
+//
+//  Created by Anthony Li on 4/20/20.
+//  Copyright © 2020 Anthony Li. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Discolor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Discolor
+//
+//  Created by Anthony Li on 4/20/20.
+//  Copyright © 2020 Anthony Li. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
